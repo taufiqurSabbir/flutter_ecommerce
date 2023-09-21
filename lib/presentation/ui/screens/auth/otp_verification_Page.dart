5266e41340f26780b08384e1f4b4aff20f75c5af
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-09-21 11:07:07 +0600

    otp dynamic count added

--- lib/presentation/ui/screens/auth/otp_verification_Page.dart
+++ lib/presentation/ui/screens/auth/otp_verification_Page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/presentation/ui/utility/AppColor.dart';
 import 'package:get/get.dart';
@@ -12,21 +14,61 @@ class OtpVerification extends StatefulWidget {
 }
 
 class _OtpVerificationState extends State<OtpVerification> {
+  int CountTime = 120;
+  late Timer timer;
+
+  void timerr(){
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      setState(() {
+        if(CountTime >0){
+          CountTime--;
+        }else{
+          t.cancel();
+        }
+      });
+    });
+  }
+
+
+  void resetTimer() {
+    setState(() {
+      CountTime = 120; // Reset countTime to its initial value
+    });
+    timer.cancel(); // Cancel the existing timer
+    timerr(); // Start a new timer
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    timerr();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              auth_page_header(
-                  title: 'Enter OTP Code',
-                  subtitle: 'A 4 Digit OTP code has been sent'),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                child: Form(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                auth_page_header(
+                    title: 'Enter OTP Code',
+                    subtitle: 'A 4 Digit OTP code has been sent'),
+                Form(
                   child: PinCodeTextField(
+
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     length: 4,
                     obscureText: false,
                     animationType: AnimationType.fade,
@@ -36,15 +78,14 @@ class _OtpVerificationState extends State<OtpVerification> {
                       borderRadius: BorderRadius.circular(5),
                       fieldHeight: 50,
                       fieldWidth: 50,
-                      activeFillColor: Colors.transparent,
+                      activeFillColor: Colors.white,
                       inactiveFillColor: Colors.white,
-                      inactiveColor: AppColors.primarycolor.withOpacity(0.5),
-                      activeColor: Colors.white,
+                      inactiveColor: AppColors.primarycolor,
+                      activeColor: AppColors.primarycolor,
                       selectedFillColor: Colors.white,
                       selectedColor: AppColors.primarycolor,
                     ),
                     animationDuration: Duration(milliseconds: 300),
-                    backgroundColor: Colors.white,
                     enableActiveFill: true,
                     cursorColor:AppColors.primarycolor,
                     validator: (String? value) {
@@ -68,37 +109,40 @@ class _OtpVerificationState extends State<OtpVerification> {
                     appContext: context,
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Get.to(OtpVerification());
-                        },
-                        child: const Text('Next'))),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              RichText(
-                text: TextSpan(children: [
-                  TextSpan(
-                      text: 'this code will expare in ',
-                      style: TextStyle(color: Colors.grey)),
-                  TextSpan(
-                      text: '120s',
-                      style: TextStyle(
-                          color: AppColors.primarycolor,
-                          fontWeight: FontWeight.bold))
-                ]),
-              ),
-              TextButton(onPressed: (){}, child: Text('Resend code',style: TextStyle(color: Colors.grey),))
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Get.to(OtpVerification());
+                          },
+                          child: const Text('Next'))),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                RichText(
+                  text: TextSpan(children: [
+                    TextSpan(
+                        text: 'this code will expare in ',
+                        style: TextStyle(color: Colors.grey)),
+                    TextSpan(
+                        text: '$CountTime s',
+                        style: TextStyle(
+                            color: AppColors.primarycolor,
+                            fontWeight: FontWeight.bold))
+                  ]),
+                ),
+
+                CountTime==0 ?TextButton(onPressed: (){
+                  resetTimer();
+                }, child: Text('Resend code',style: TextStyle(color: Colors.grey),)) : SizedBox(height: 1,)
+              ],
+            ),
           ),
         ),
       ),
