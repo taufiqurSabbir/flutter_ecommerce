dd2b61ca71bd560bea7dfcd439fbc13fac6e749e
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-09-21 11:36:03 +0600

    form theme adjust

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -13,21 +13,32 @@ class craftBay extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Splash_Screen(),
       theme: ThemeData(
-          // primaryColor: Color(0xff07ADAE),
-          primarySwatch:
-              MaterialColor(AppColors.primarycolor.value, AppColors().color),
-          progressIndicatorTheme: ProgressIndicatorThemeData(
-              // color:  Color(0xff07ADAE)
-              ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
+        // primaryColor: Color(0xff07ADAE),
+        primarySwatch:
+            MaterialColor(AppColors.primarycolor.value, AppColors().color),
+        progressIndicatorTheme: ProgressIndicatorThemeData(
+            // color:  Color(0xff07ADAE)
+            ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
             padding: const EdgeInsets.symmetric(vertical: 15),
             textStyle: const TextStyle(
                 fontSize: 16, fontWeight: FontWeight.w600, letterSpacing: 0.5),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
-          ))),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.primarycolor)),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.primarycolor)),
+          disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.primarycolor)),
+        )
+      ),
     );
   }
 }
