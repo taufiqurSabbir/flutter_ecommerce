651097af4e14256915e5fcf776540e8d2969c03c
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-22 20:08:08 +0600

    body text

---
+++ lib/presentation/ui/widgets/Body_text.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class BodyTitle extends StatelessWidget {
+  const BodyTitle({
+    super.key, required this.title_text, required this.TextColor, required this.textsize,
+  });
+  final String title_text;
+  final Color TextColor;
+  final double textsize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Text(
+        title_text,
+        style: TextStyle(
+          fontSize: textsize,
+          fontWeight: FontWeight.bold,
+          color: TextColor,
+        ),
+      ),
+    );
+  }
+}
