fc87baa3ab872269221bcef252c809e61bde120a
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-22 20:09:47 +0600

    product widget

--- lib/presentation/ui/utility/image_asset.dart
+++ lib/presentation/ui/utility/image_asset.dart
@@ -1,4 +1,5 @@
 class ImmageAsset{
   static final String logosvg = 'assets/images/logo.svg';
   static final String navlogo = 'assets/images/logo_nav.svg';
+  static final String bed_icon = 'assets/images/bed.svg';
 }
---
+++ lib/presentation/ui/widgets/ProductWidget.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import '../utility/AppColor.dart';
+
+class ProductWidget extends StatelessWidget {
+  const ProductWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Column(
+        children: [
+          Container(
+            height: 230,
+            width: 200,
+            decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12.withOpacity(0.01),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3)
+                  ),
+                ],
+                color: AppColors.primarycolor.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(20)
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: 140,
+                  width: 200,
+                  color: Colors.transparent,
+                  child: Center(
+                    child: Image.network('https://www.pngall.com/wp-content/uploads/5/Purse.png',width: 120,height: 150,),
+                  ),
+                ),
+
+                Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+
+                      borderRadius: BorderRadius.only(topLeft: Radius.zero,topRight:Radius.zero,bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20) )
+                  ),
+                  height: 90,
+                  width: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Ledies trendy Purse',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
+                        SizedBox(height: 5,),
+                        Row(
+                          children: [
+                            Spacer(),
+                            Text('2700 ৳',style: TextStyle(fontSize: 20,color: AppColors.primarycolor,fontWeight: FontWeight.bold),),
+                            Spacer(),
+                            Icon(Icons.star,color: Colors.amber,),
+                            Text('4.3',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),
+                            Spacer(),
+                            Icon(Icons.favorite,color: AppColors.primarycolor,),
+                            Spacer(),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+
+                ),
+
+              ],
+            ),
+          ),
+          SizedBox(height: 20,)
+        ],
+      ),
+    );
+  }
+}
