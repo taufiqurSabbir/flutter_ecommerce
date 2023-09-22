d92d327f964efb1f78be400cc087bd2d4e8353cb
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-22 20:35:18 +0600

    icon added to asset function

--- lib/presentation/ui/utility/image_asset.dart
+++ lib/presentation/ui/utility/image_asset.dart
@@ -2,4 +2,7 @@ class ImmageAsset{
   static final String logosvg = 'assets/images/logo.svg';
   static final String navlogo = 'assets/images/logo_nav.svg';
   static final String bed_icon = 'assets/images/bed.svg';
+  static final String food_icon = 'assets/images/food.svg';
+  static final String fashion_icon = 'assets/images/fashion.svg';
+  static final String electronics_icon = 'assets/images/electronics.svg';
 }
