cc017924f81c11b51d29b85afd805dc99a6af51c
Nayon Talukder <69708059+nayon-coders@users.noreply.github.com>
2023-09-25 17:49:01 +0600

    issus solve

--- lib/presentation/ui/utility/image_asset.dart
+++ lib/presentation/ui/utility/image_asset.dart
@@ -1,4 +1,4 @@
-class ImmageAsset{
+class ImageAsset{
   static final String logosvg = 'assets/images/logo.svg';
   static final String navlogo = 'assets/images/logo_nav.svg';
   static final String bed_icon = 'assets/images/bed.svg';
